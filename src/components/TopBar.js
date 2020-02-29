c0aa685bf4e4dcc2efd4fa27c50cbd4f4ce4bae6
Volodymyr <462960@gmailcom>
2020-02-29 10:59:51 +0200

    UI prepeared

---
+++ src/components/TopBar.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import GitHubIcon from "./GitHubIcon";
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+const linkGitHub = "https://github.com/462960/hawkWeather";
+
+export default function TopBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            href={linkGitHub}
+          >
+            <GitHubIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            JSON
+          </Typography>
+          {/* <Button color="inherit">Login</Button> */}
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
+}
